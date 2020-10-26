817ee762a88b3c48c0c851747036b2936a4cc7d5
orion160 <idbl64@outlook.com>
2020-10-25 21:44:37 -0500

    Modernized pointer cast

--- Computacion_grafica_y_OpenGL/Computacion_grafica_y_OpenGL.cpp
+++ Computacion_grafica_y_OpenGL/Computacion_grafica_y_OpenGL.cpp
@@ -121,7 +121,7 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), reinterpret_cast<void*>(0));
 	glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window))
